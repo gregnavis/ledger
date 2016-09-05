619751bf1ba9a4ec948cf3866718d174acf2f793
Greg Navis <contact@gregnavis.com>
2016-09-05 12:05:05 +0200

    Creation and retrieval of accounts

--- ledger.py
+++ ledger.py
@@ -1,6 +1,46 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+
+class Database(object):
+    def __init__(self):
+        self.accounts = []
+
+    def create_account(self, code, name):
+        '''Create an account with a given code and name.'''
+        code = str(code)
+        name = str(name)
+
+        if self.get_account(code):
+            return RuntimeError('The account {} already exists'.format(code))
+
+        self.accounts.append({'code': code, 'name': name, 'balance': 0})
+
+    def get_account(self, code):
+        '''Return the account identified by the specified code.'''
+        code = str(code)
+
+        for account in self.accounts:
+            if code == account['code']:
+                return account
+        else:
+            return None
+
+
+database = Database()
+
+
+@app.route('/accounts/<code>', methods=['GET'])
+def get_account(code):
+    return jsonify(database.get_account(code))
+
+
+@app.route('/accounts', methods=['POST'])
+def create_account():
+    database.create_account(request.json['code'], request.json['name'])
+    return 'Created'
+
 
 if __name__ == '__main__':
     app.run()
--- ledger_test.py
+++ ledger_test.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 
 import ledger
@@ -7,6 +8,24 @@ class LedgerTestCase(unittest.TestCase):
     def setUp(self):
         ledger.app.config['TESTING'] = True
         self.app = ledger.app.test_client()
+
+    def test_create_account(self):
+        self._create_account('101', 'Cash')
+        self.assertEqual(
+            {'name': 'Cash', 'code': '101', 'balance': 0},
+            self._get_account('101')
+        )
+
+    def _create_account(self, code, name):
+        response = self.app.post('/accounts', content_type='application/json',
+                                 data=json.dumps({'code': code, 'name': name}))
+        self.assertEqual(200, response.status_code)
+
+    def _get_account(self, code):
+        response = self.app.get('/accounts/{}'.format(code))
+        self.assertEqual(200, response.status_code)
+        self.assertEqual('application/json', response.content_type)
+        return json.loads(response.data)
 
 
 if __name__ == '__main__':
